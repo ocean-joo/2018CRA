ba9131145bf7828bc5fee7def59dae0fc0ea7b06
Hyewon Jeon <bes4u@naver.com>
2018-07-10 10:34:28 +0900

    add moive rating

--- movieReview.py
+++ movieReview.py
@@ -5,17 +5,18 @@ Created on Fri Jul  6 13:19:50 2018
 @author: hyeOn
 """
  
-def moiveReview(moive_name, maxPage=10) :
-    import urllib
+def movieReview(movie_name, maxPage=10) :
+    
+    import urllib 
 
     client_ID = 'iPiLUtbmWB3Ml5f8Ay3A'
     client_secret = 'WH0MRvrfmm'
-    encoded_moive_name = urllib.parse.quote(moive_name)
+    encoded_movie_name = urllib.parse.quote(movie_name)
     
     query = "https://openapi.naver.com/v1/search/movie.json"
     option = "?display=1&yearfrom=2000&yearto=2018&query="
     
-    url_query = query + option + encoded_moive_name
+    url_query = query + option + encoded_movie_name
     
     request = urllib.request.Request(url_query)
     request.add_header("X-Naver-Client-Id",client_ID)
@@ -26,7 +27,7 @@ def moiveReview(moive_name, maxPage=10) :
     
     if rescode != 200 :
         print("Error Code : " + str(rescode))
-        
+    
     import json
     item = json.loads(response.read())
     url = item['items'][0]['link']
@@ -40,21 +41,25 @@ def moiveReview(moive_name, maxPage=10) :
     import requests
     from bs4 import BeautifulSoup
 
-    result = []
+    result = tuple()
 
     for j in range(1,maxPage+1) :
         inputStr = str1 + str2 + str3 + '&page=' + str(j)
         html = requests.get(inputStr).text
         soup = BeautifulSoup(html, 'html.parser')
         
-        text_list = soup.select('body > div > div > div.score_result > ul div.score_reple > p')
-        
-        for i in text_list :
-            temp = i.text
-            temp = temp.rstrip()
+        text_list = (soup.select('body > div > div > div.score_result > ul div.score_reple > p'),
+				soup.select("body > div > div > div.score_result > ul div.star_score > em"))
+        for i, k in text_list :
+            temp[0] = i.text
+            temp[0] = temp[0].rstrip()
+            if temp[0].startwith("관람객") :
+                temp[0] = temp[0].split("관람객")[0]
+            temp[1] = k.text
             result.append(temp)
-    
     
     return result
 
-
+movieReview("강철비")
+movieReview("신과 함께")
+movieReview("1987")
