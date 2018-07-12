7931618295ee6196eb2eb1bf0d5340276288abc9
Hyewon Jeon <bes4u@naver.com>
2018-07-12 14:04:14 +0900

    Make list of total movies

---
+++ ListofMovie.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import movieReview
+
+def get_movie_list() :
+    list12 = pd.read_excel("2012BO.xlsx")
+    list13 = pd.read_excel("2013BO.xlsx")
+    list14 = pd.read_excel("2014BO.xlsx")
+    list15 = pd.read_excel("2015BO.xlsx")
+    list16 = pd.read_excel("2016BO.xlsx")
+    list17 = pd.read_excel("2017BO.xlsx")
+    list18 = pd.read_excel("2018BO.xlsx")
+    
+    movie_name_list = list(list12['영화명']) + list(list13['영화명']) + list(list14['영화명']) + list(list15['영화명']) + list(list16['영화명']) + list(list17['영화명']) + list(list18['영화명'])
+    
+    movie_year_list = list(list12['개봉일']) + list(list13['개봉일']) + list(list14['개봉일']) + list(list15['개봉일']) + list(list16['개봉일']) + list(list17['개봉일']) + list(list18['개봉일'])
+    
+    movie_year_list = [str(i)[:4] for i in movie_year_list]
+    
+    movie_list = list(zip(movie_name_list, movie_year_list))
+
+    len(movie_list) # 700
+    movie_list = list(set(movie_list))
+    len(movie_list) # 675
+    
+    return movie_list
+
+
+
